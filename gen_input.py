809d90d153ea76b1feb29c756196adf46fe5c55c
Xuthree <xuweichenahu@163.com>
2023-11-19 06:45:15 +0800

    Revert "Initial commit"
    This reverts commit 44c63f57b1c554bcdee7727e542c5b79cebd8177.

--- gen_input.py
+++ gen_input.py
@@ -1,9 +1,7 @@
 # from collections import defaultdict
 from typing import Optional
-import re
 
 
-# 将p中参数改为数据类，标记
 class Genincar:
     p = {  # dictionary to hold the routine parameters of incar file
         'str_keys': {
@@ -19,26 +17,19 @@ class Genincar:
         'int_keys': {
             'ispin',  # Spin polarization
             'ibrion',  # ionic relaxation: 0-MD 1-quasi-New 2-CG
-            'icharg',  # charge: 0-WAVECAR 1-CHGCAR 2-atom 10-const
+            'icharge',  # charge: 0-WAVECAR 1-CHGCAR 2-atom 10-const
             'isif',  # calculate stress and what to relax
             'ivdw',  # Choose which dispersion correction method to use
             'ismear',  # part. occupancies: -5 Blochl -4-tet -1-fermi 0-gaus >0 MP
             'ispin',  # spin-polarized calculation
             'istart',  # startjob: 0-new 1-cont 2-samecut
-            'idipol',  # monopol/dipol and quadropole corrections
             'nsw',  # number of steps for ionic relaxation
-            'nelm',
-            'nelmin',  # Maximum number of electronic steps
+            'nelm',  # Maximum number of electronic steps
         },
 
         'bool_keys': {
             'lcharg',  # .true. to calculate charge density
             'lwave',  # .true. to write WAVECAR
-            'ldipol',  # potential correction mode
-            'lsorbit',  # Enable spin-orbit coupling
-            'lsol',  # turn on solvation for Vaspsol
-            'ldau',
-            'addgrid',  # L(S)DA+U
         },
 
         'exp_keys': {
@@ -47,21 +38,15 @@ class Genincar:
         },
         'special_keys': {
             'lreal',  # non-local projectors in real space
-        },
-        'klists': {
-            'dipol',  # center of cell for dipol
-            'magmom',  # initial magnetic moments
-            'ldauu',  # ldau parameters, has potential to redundant w.r.t. dict
-            'ldauj',  # key 'ldau_luj', but 'ldau_luj' can't be read direct from
         }
     }
 
     default_incar_params = {
         'istart': 0,
         'ispin': 1,
-        'icharg': 2,
+        'icharge': 2,
         'lreal': 'Auto',
-        'encut': 500,
+        'encut': 400,
         'prec': 'Normal',
         'algo': 'Fast',
         'lcharg': False,
@@ -84,7 +69,6 @@ class Genincar:
 
         for key, value in kwargs.items():
             self.params[key] = value
-        self.all_keys = [i for value in self.p.values() for i in value]
 
     def _convert(self, key, value):
         if key in self.p['float_keys']:
@@ -96,7 +80,7 @@ class Genincar:
         elif key in self.p['str_keys']:
             val = str(value)
         elif key in self.p['exp_keys']:
-            val = f'{float(value) :5.2e}'
+            val = f'{(value):5.2e}'
         elif key in self.p['special_keys']:
             if isinstance(value, bool):
                 val = f'.{value}.'
@@ -107,8 +91,9 @@ class Genincar:
         return val
 
     def add_pa(self, **kwargs):
+        all_pa = (i for value in self.p.values() for i in value)
         for key in kwargs:
-            if key in self.all_keys:
+            if key in all_pa:
                 self.params[key] = kwargs[key]
             else:
                 raise Exception(f'{key} parameter is not defined')
@@ -122,40 +107,3 @@ class Genincar:
         incar_dict = self._get_params()
         key_value = [f'{k} = {v}' for k, v in incar_dict.items()]
         return 'Globle Parameters\n' + '\n'.join(key_value)
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
-    def __getitem__(self, item):
-        if item in self.all_keys:
-            return self.params[item]
-        else:
-            raise Exception(f'{item} is not a valid key')
-
-    def read_pa(self, path):
-        with open(path, 'r') as f:
-            lines = f.readlines()
-        for line in lines:
-            if line.startswith('#') or ('=') not in line:
-                continue
-
-            key, value = ' '.join(re.findall(r'\w*\s*=\s*\.*\W*\w*\-*\d*', line)).replace(' ', '').split('=')
-            self.add_pa(**{key.lower(): value})
-
-    def wtite_pa(self, path):
-        with open(path, 'w') as f:
-            f.write('Globle Parameters\n')
-            for key, value in self.params.items():
-                f.write(f'{key} = {value}\n')
-
-
-
-
-
-# def main():
-#     incar = Genincar()
-#     incar.read_pa(r'D:\Desktop\temp\INCAR')
-#     incar.add_pa(nsw=0)
-#     incar.wtite_pa(r'D:\Desktop\temp\caca\INCAR')
-#     print(incar['NSW'])
-# main()
